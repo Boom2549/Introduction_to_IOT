f8550310fbefed6f4f324138b8d55ca9ca18e939
IOT-engineer <67011063@kmitl.ac.th>
2024-07-23 23:39:54 +0700

    adding the rest of it

---
+++ Assignment/week2/23/23.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}c:\Users\hp\Documents\GitHub\Introduction_to_IOT\Assignment\week2\23.c
+
+void loop() {
+  // put your main codc:\Users\hp\Documents\GitHub\Introduction_to_IOT\Assignment\week2\23.c c:\Users\hp\Documents\GitHub\Introduction_to_IOT\Assignment\week2\analog_19.ce here, to run repeatedly:
+c:\Users\hp\Documents\GitHub\Introduction_to_IOT\Assignment\week1\led_12.c
+}
